79d3169d69c2f20bb422e2809df83f100d2dda2a
Bobur Yusupov <boburinfo2006@gmail.com>
2023-06-20 11:40:28 +0500

    Added Permissions

--- blog-api/posts/views.py
+++ blog-api/posts/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAdminUser
+from .permissions import IsAuthorOrReadOnly
 
 from .models import Post
 from .serializers import PostSerializer
 
 # Create your views here.
 class PostList(ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
