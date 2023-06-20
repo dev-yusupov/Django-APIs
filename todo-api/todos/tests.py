cfac7d32f75b313c69b0c1e1291bf7383ff540b8
Bobur Yusupov <boburinfo2006@gmail.com>
2023-06-20 08:59:29 +0500

    API tests added to TODO-API

--- todo-api/todos/tests.py
+++ todo-api/todos/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
 
 from .models import Todo #importing Todo model
 
@@ -15,3 +17,19 @@ class TestTodoModel(TestCase):
         self.assertEqual(self.todo.title, "Test Todo")
         self.assertEqual(self.todo.body, "Test body of todo")
         self.assertEqual(str(self.todo), "Test Todo")
+
+    def test_api_listview(self):
+        response = self.client.get(reverse("todo_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, self.todo)
+    
+    def test_api_detail(self):
+        response = self.client.get(
+            reverse("todo_detail", kwargs={"pk": self.todo.id}),
+            format="json"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, "Test Todo")
